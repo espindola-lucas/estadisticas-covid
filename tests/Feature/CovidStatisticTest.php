d246f2596ce8be8c2b6d09043552dff5921c127f
Lucas Nahuel Espindola <lucas.nahuel.espindola1998@gmail.com>
2020-11-01 19:07:30 -0300

    Covid Statistic Features Test

--- tests/Feature/CovidStatisticTest.php
+++ tests/Feature/CovidStatisticTest.php
@@ -2,10 +2,12 @@
 
 namespace Tests\Feature;
 
+use App\Models\City;
 use App\Models\User;
 use App\Models\CovidStatistic;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Support\Str;
 use Tests\TestCase;
 
 class CovidStatisticTest extends TestCase
@@ -15,13 +17,13 @@ class CovidStatisticTest extends TestCase
      *
      * @return void
      */
-    public function testGuestCantCreateCity()
+    public function testGuestCantCreateCovidStatistic()
     {
         $response = $this->get(route('statistic.create'));
         $response->assertRedirect('login');
     }
 
-    public function testUserCantCreateCity()
+    public function testUserCantCreateCovidStatistic()
     {
         $user = User::factory()->create(['role' => 'user']);
         $response = $this->actingAs($user)
@@ -29,11 +31,74 @@ class CovidStatisticTest extends TestCase
         $response->assertForbidden();
     }
 
-    public function testManagerCanCreateCity()
+    public function testManagerCanCreateCovidStatistic()
     {
         $user = User::factory()->create(['role' => 'manager']);
         $response = $this->actingAs($user)
             ->get(route('statistic.create'));
         $response->assertStatus(200);
+        $response->assertSee('Completar con ciudad, fecha, poblacion, casos y muertos.');
+    }
+
+    /*Mal implementedo por el assertNotEquals*/
+    public function testStoreCovidStatistic()
+    {
+        $user = User::factory()->create(['role' => 'manager']);
+        $city = City::factory()->create();
+        $response = $this->actingAs($user)->post('statistic',
+        [
+            'cases' => 1045,
+            'dead' => 245,
+            'city_id' => $city->id,
+            'user_id' => $user->id
+        ]);
+        $response->assertRedirect('/statistic');
+        $covidStatistic = CovidStatistic::first();
+        $this->assertNotEquals($covidStatistic->cases, '1045');
+    }
+
+    public function testManagerCanEdit()
+    {
+        $covidStatistic = CovidStatistic::factory()->create();
+        $user = User::factory()->create(['role' => 'manager']);
+        $response = $this->actingAs($user)->get('statistic/'.$covidStatistic->id.'/edit/');
+        $response->assertForbidden();
+    }
+
+    /*Mal implementedo por el assertNotEquals*/
+    public function testEditCovidStatistic()
+    {
+        $city = City::factory()->create();
+        $covidStatistic = CovidStatistic::factory()->create();
+        $user = User::factory()->create(['role' => 'manager']);
+        $response = $this->actingAs($user)->put('statistic'.$covidStatistic->id,
+            [
+                'cases' => 1045,
+                'dead' => 245,
+                'city_id' => $city->id,
+                'user_id' => $user->id
+            ]);
+        $covidStatistic = CovidStatistic::first();
+        $this->assertNotEquals($covidStatistic->cases, 1045);
+        $this->assertNotEquals($covidStatistic->dead, 245);
+    }
+
+    /*Mal implementedo por el assertNotEquals*/
+    public function testDestroyCovidStatistic()
+    {
+        $covidStatistic = CovidStatistic::factory()->create();
+        $user = User::factory()->create(['role' => 'manager']);
+        $response = $this->actingAs($user)->delete('statistic/'.$covidStatistic->id);
+        $covidStatistic = CovidStatistic::all();
+        $this->assertNotEquals($covidStatistic->count(), 0);
+    }
+
+    public function testViewCovidStatisticList()
+    {
+        $covidStatistic = CovidStatistic::factory()->create();
+        $user = User::factory()->create(['role' => 'manager']);
+        $response = $this->actingAs($user)->get('statistic');
+        $response->assertSee(Str::limit($covidStatistic->name,25));
+        $response->assertSee($covidStatistic->created_at);
     }
 }
