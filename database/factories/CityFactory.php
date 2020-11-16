25f3793148218d185c4667cebde2e031a95c9424
Ariel Hidalgo <arielhidalgo808@gmail.com>
2020-11-16 14:26:10 -0300

    Replacing static access on cityFactory

--- database/factories/CityFactory.php
+++ database/factories/CityFactory.php
@@ -3,7 +3,6 @@
 namespace Database\Factories;
 
 use App\Models\City;
-use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 class CityFactory extends Factory
@@ -22,7 +21,6 @@ class CityFactory extends Factory
      */
     public function definition()
     {
-        $user = User::factory()->create();
         return [
             'name'=>$this->faker->country,
             'population'=>$this->faker->numberBetween(1,1000000),
