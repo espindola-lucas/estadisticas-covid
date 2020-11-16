745c838e86913a2bdef95c4740630f3523a1bcdd
Ariel Hidalgo <arielhidalgo808@gmail.com>
2020-11-16 17:08:51 -0300

    Data expose JSON (#72) (Closes #69)
    * Create Json test for cities visualization
    
    * Fixed Json response on CityApiTest
    
    * Add 'user can´t read unassigned cities' test
    
    * Change Json response to see all the cities
    
    * Add HTML + JS archives for data visualization
    
    * Add CSS to JSON data visualization (close #19)
    
    * Add CSS to JSON data visualization (close #69)
    
    * Delete unused 'asigned to' atributte on city table
    
    * Modify cityApiTest
    
    * Modify DatabaseSeeder
    
    * Delete unused files
    
    * Remove unused parameters on apiController
    
    * Replacing static access on cityFactory
    
    * Replacing static access on cityApiTest
    
    * Change sintax on ApiController
    
    * Change sintax on apiController
    
    * Change cityApiTest sintax
    
    * Change cityApiTest syntax
    
    * Deleting repeated file

---
+++ tests/Feature/CityApiTest.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Models\User;
+use App\Models\City;
+use Laravel\Sanctum\Sanctum;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class CityApiTest extends TestCase
+{
+    /**
+     * A basic feature test example.
+     *
+     * @return void
+     */
+    public function testUserCanViewCities()
+    {
+        $user = User::factory()->create();
+        $city = City::factory()->create();
+
+        $response = $this->actingAs($user)->getJson('/api/cities');
+
+        $response->assertStatus(200);
+        $response->assertJsonFragment([
+            "name" => $city->name
+        ]);
+    }
+}
