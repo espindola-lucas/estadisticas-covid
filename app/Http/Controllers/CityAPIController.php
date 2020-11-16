2dc2cbcffbdd1ef8476a555dce49de51100102c7
Ariel Hidalgo <arielhidalgo808@gmail.com>
2020-11-16 14:24:45 -0300

    Remove unused parameters on apiController

--- app/Http/Controllers/CityAPIController.php
+++ app/Http/Controllers/CityAPIController.php
@@ -2,15 +2,14 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\City;
 use Illuminate\Http\Request;
 
 class CityAPIController extends Controller
 {
     //
 
-    public function index(Request $request){
-        $cities = City::all();
+    public function index(){
+        $cities = \App\Models\City::all();
 
         return response()->json($cities);
     }
