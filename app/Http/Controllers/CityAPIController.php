5b3e4231ee3512940651718f7c34ba57e8183821
Ariel Hidalgo <arielhidalgo808@gmail.com>
2020-11-16 14:58:04 -0300

    Change sintax on ApiController

--- app/Http/Controllers/CityAPIController.php
+++ app/Http/Controllers/CityAPIController.php
@@ -3,14 +3,22 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\City;
 
 class CityAPIController extends Controller
 {
     //
-
+    /*
     public function index(){
         $cities = \App\Models\City::all();
 
         return response()->json($cities);
+    } */
+    public function city(){
+        return City::all();
+    }
+
+    public function index(){
+        return response()->json(city());
     }
 }
