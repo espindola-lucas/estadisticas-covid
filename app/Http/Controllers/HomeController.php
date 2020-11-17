482b5866555460c4385fd915a8bf5f339e760c1e
Lucas Nahuel Espindola <lucas.nahuel.espindola1998@gmail.com>
2020-11-17 08:18:48 -0300

    Deleting unnecessary files

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,92 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\City;
+use App\Models\CovidStatistic;
+use Carbon\Carbon;
+use Illuminate\Http\Request;
+
+class HomeController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $cities = City::with('statistics')->get();
+        $statistics = CovidStatistic::with('city')->orderBy('created_at', 'DESC')->get();
+        return view('welcome', [
+            'cities' => $cities,
+            'statistics' => $statistics,
+            ]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\City  $city
+     * @return \Illuminate\Http\Response
+     */
+    public function show(City $city)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\City  $city
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(City $city)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\City  $city
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, City $city)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\City  $city
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(City $city)
+    {
+        //
+    }
+}
