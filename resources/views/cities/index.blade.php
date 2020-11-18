f49d075c9b452afb12e6d070a52b07d682971979
Lucas Nahuel Espindola <lucas.nahuel.espindola1998@gmail.com>
2020-11-18 20:16:52 -0300

    Small Changes

--- resources/views/cities/index.blade.php
+++ resources/views/cities/index.blade.php
@@ -3,6 +3,7 @@
     <div class="mb-6 lg:flex lg:items-center lg:justify-between">
         <div class="flex-1 min-w-0">
             <h2 class="text-2xl font-bold leading-7 text-gray-900 sm:text-3xl sm:leading-9 sm:truncate">
+            Ciudades recientes
             </h2>
         </div>
         <div class="mt-5 flex lg:mt-0 lg:ml-4">
