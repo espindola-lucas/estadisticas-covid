99c741d4845821f1c8eae72ae73779804a63709e
Lucas Nahuel Espindola <lucas.nahuel.espindola1998@gmail.com>
2021-01-04 20:54:00 -0300

    Change Title

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,7 +6,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title>Covid-19</title>
+    <title>Coronavirus</title>
   </head>
 
   <body class="antialiased">
